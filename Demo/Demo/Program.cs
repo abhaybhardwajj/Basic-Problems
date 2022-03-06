df8545b7ad08f7985671b360cd1c4585cd4bf397
abhaybhardwajj <abhay20021998@gmail.com>
2022-03-06 22:52:14 +0530

    Basic Problems

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Enter Year : ");
+int Year = int.Parse(Console.ReadLine());
+
+if (((Year % 4 == 0) && (Year % 100 == 0)) || (Year % 400 == 0))
+{
+      
+    Console.WriteLine("(0) is a Leap year." Year);
+
+}
+else
+{
+
+    Console.WriteLine("(0) is not a Leap year." Year);
+    Console.ReadLine();
+}
