39118644fd11904c1456a57de2ad616a9935b14f
abhaybhardwajj <abhay20021998@gmail.com>
2022-03-06 22:48:45 +0530

    Leap Year

---
+++ leap Year/leap Year/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Leap year");
+
+    Console.WriteLine("Enter Year : "); // constant
+     int Year = int.Parse(Console.ReadLine());
+
+if (((Year % 4 == 0) && (Year % 100 == 0)) || (Year % 400 == 0))
+{
+    Console.WriteLine("{0} is a Leap year.", Year);
+
+}
+else
+{
+
+    Console.WriteLine("{0} is not a Leap year.", Year);
+    Console.ReadLine();
+}
